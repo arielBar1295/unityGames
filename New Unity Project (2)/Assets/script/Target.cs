bcf2cf0fd1f450886d30fb88bbdf51dc5c140d4d
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2020-04-26 11:48:00 +0300

    add Ex1

---
+++ New Unity Project (2)/Assets/script/Target.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Target : MonoBehaviour 
+ {
+ 
+     private float RotateSpeed = 3f;
+     private float Radius = 2.0f;
+ 
+     private Vector2 centre;
+     private float angle;
+ 
+     private void Start()
+     {
+         centre = transform.position;
+     }
+ 
+     private void Update()
+     {  //continue moving while holding the key
+        if (Input.GetKey(KeyCode.RightArrow)) {
+           angle += RotateSpeed * Time.deltaTime;
+        } else if (Input.GetKey(KeyCode.LeftArrow)) {
+             angle -= RotateSpeed * Time.deltaTime;
+        }
+ 
+        
+ 
+         var offset = new Vector2(Mathf.Sin(angle), Mathf.Cos(angle)) * Radius;
+         transform.position = centre + offset;
+     }
+  
+  
+ 
+ }
---
+++ New Unity Project (2)/Assets/script/gui1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gui1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    void OnGUI()
+    {
+    GUI.Box(new Rect(700, 20, 100, 20), "Score : "+ GameObject.Find("player1").GetComponent<control2Players>().pointP1);
+
+    }
+}
