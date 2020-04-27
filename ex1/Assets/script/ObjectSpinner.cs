6287580718d4c5dc412745ce7cdfa88d76f61e1f
Or Avital <oravital7@gmail.com>
2020-04-27 20:45:48 +0300

    Added oscillator

---
+++ ex1/Assets/script/ObjectSpinner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectSpinner : MonoBehaviour
+{
+     public float speed=50.0f;
+     public Transform target;
+     
+     private Vector3 zAxis = new Vector3(0, 0, 1);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.RotateAround(target.position, zAxis, speed);
+        transform.Rotate(Vector3.up * speed * Time.deltaTime);
+    }
+}
---
+++ ex1/Assets/script/Oscillator.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+
+    public float velocityThreshold;
+    public float rightPushRange;
+    public float leftPushRange;
+    public Rigidbody2D body2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body2d = GetComponent<Rigidbody2D>();
+        body2d.angularVelocity = velocityThreshold;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        oscillator();
+    }
+
+    void oscillator()
+    {
+        if (transform.rotation.z > 0
+            && transform.rotation.z < rightPushRange
+            && (body2d.angularVelocity > 0)
+            && body2d.angularVelocity < velocityThreshold)
+        {
+            body2d.angularVelocity = velocityThreshold;
+        }
+        else if (transform.rotation.z < 0
+            && transform.rotation.z > leftPushRange
+            && (body2d.angularVelocity < 0)
+            && body2d.angularVelocity > velocityThreshold * -1)
+        {
+            body2d.angularVelocity = velocityThreshold * -1;
+        }
+    }
+}
