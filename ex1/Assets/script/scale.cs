f945d29c519eef4b7890de10fed33368dfadbb13
arielBar1295 <44972743+arielBar1295@users.noreply.github.com>
2020-04-27 17:30:44 +0300

    Ex1

---
+++ ex1/Assets/script/scale.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scale : MonoBehaviour
+{
+
+   Vector3 scaleChange = new Vector3(0.01f, 0.01f, 0.01f);
+ public void Update(){
+    
+     transform.localScale +=scaleChange;
+      if (transform.localScale.y < 0.7f || transform.localScale.y > 1.0f)
+        {
+            scaleChange = -scaleChange;
+        }
+}
+}
