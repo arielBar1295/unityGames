bdad57d8f930a48d50a8ddcb11e19ef863cc5b70
Dana Mor <dandimor@gmail.com>
2020-05-29 18:11:17 +0300

    timer'

---
+++ drone/Assets/script/Countdown.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Countdown : MonoBehaviour
+{
+    [SerializeField] int levelTolLoad;
+    [SerializeField] float timer;
+    [SerializeField] Text timerSeconds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timerSeconds = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        timerSeconds.text = timer.ToString("f2");
+        if(timer<=0)
+        {
+            Debug.Log("in if!!");
+            Application.LoadLevel(levelTolLoad);
+        }
+    }
+}
